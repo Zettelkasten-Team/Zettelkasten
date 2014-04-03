caec1cb063d89335f1a69eaf54279ca878fa8ec1
Daniel Lüdecke <d.luedecke@uke.de>
2014-04-03 22:46:52 +0200

    Release 3.2.4.1

--- src/de/danielluedecke/zettelkasten/util/Constants.java
+++ src/de/danielluedecke/zettelkasten/util/Constants.java
@@ -49,7 +49,7 @@ public class Constants {
     /**
      * This variable stores the current programme and build version number
      */
-    public static final String BUILD_VERSION = "3.2.5 nightly (Build 20140111)";
+    public static final String BUILD_VERSION = "3.2.4.1 (Build 20140404)";
     /**
      * This constants stores the website-address where the Zettelkasten can be downloaded:<br><br>
      * {@code http://zettelkasten.danielluedecke.de/download.php}
