07987cba6c3db326ec5510a14933ddb9189411e8
Elias John <elias.john@isiconsult.net>
2020-04-10 23:01:06 +0200

    activating anti aliasing for text with value lcd by default

--- src/main/java/de/danielluedecke/zettelkasten/ZettelkastenApp.java
+++ src/main/java/de/danielluedecke/zettelkasten/ZettelkastenApp.java
@@ -79,6 +79,7 @@ public class ZettelkastenApp extends SingleFrameApplication {
      * @param args
      */
     public static void main(String[] args) {
+        System.setProperty("Dawt.useSystemAAFontSettings", "lcd");
         launch(ZettelkastenApp.class, args);
     }
 
