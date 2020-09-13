df77788cd58b4e59f75c00f709f26a50d0751956
Ralf Barkow <>
2020-09-13 11:02:21 +0200

    Add native2ascii Python script file

--- translations/pt_BR/native2ascii.py
+++ translations/pt_BR/native2ascii.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python3
+
 import os
 
 rootdir = r'/Users/rgb/Projects/Zettelkasten/src/main/resources/de/danielluedecke/zettelkasten/'
@@ -7,4 +9,5 @@ for subdir, dirs, files in os.walk(rootdir):
         filepath = subdir + os.sep + file
 
         if filepath.endswith("_pt_BR.properties"):
+            print('{}'.format(repr(filepath)))
             os.system("native2ascii -encoding 8859_1 " + filepath + " " + filepath)
