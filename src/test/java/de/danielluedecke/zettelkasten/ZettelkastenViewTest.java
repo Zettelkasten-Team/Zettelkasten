6d564aa7d6b249a09fce27c50dd9c1b95287477d
Ralf Barkow <>
2020-11-07 14:15:27 +0100

    test (ZettelkastenViewTest): /* TODO Add at least one assertion to this test case. */

---
+++ src/test/java/de/danielluedecke/zettelkasten/ZettelkastenViewTest.java
@@ -0,0 +1,8 @@
+package de.danielluedecke.zettelkasten;
+public class ZettelkastenViewTest {
+
+    @org.junit.Test
+    public void editEntry() {
+        /* TODO Add at least one assertion to this test case. */
+    }
+}
