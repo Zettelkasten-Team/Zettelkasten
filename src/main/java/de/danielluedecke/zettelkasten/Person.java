7753d0431d9f65e458f07f8f1e7f0091e4af3bbb
Ralf Barkow <ralf.barkow@me.com>
2020-11-12 15:54:18 +0100

    test (Person): replace with the code from  Eric M. Burke's Java extreme programming cookbook

--- src/main/java/de/danielluedecke/zettelkasten/Person.java
+++ src/main/java/de/danielluedecke/zettelkasten/Person.java
@@ -1,28 +1,29 @@
 package de.danielluedecke.zettelkasten;
 
 public class Person {
-
     private String firstName;
     private String lastName;
 
     public Person(String firstName, String lastName) {
+        if (firstName == null && lastName == null) {
+            throw new IllegalArgumentException("Both names cannot be null");
+        }
         this.firstName = firstName;
         this.lastName = lastName;
+    }
+
+    public String getFullName() {
+        String first = (this.firstName != null) ? this.firstName : "?";
+        String last = (this.lastName != null) ? this.lastName : "?";
+
+        return first + " " + last;
     }
 
     public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
+        return this.firstName;
     }
 
     public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
+        return this.lastName;
     }
 }
