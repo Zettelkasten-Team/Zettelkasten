17e448d4fdeea615dd08a4f1085c47ba97c898e0
Ralf Barkow <ralf.barkow@me.com>
2020-11-18 10:54:45 +0100

    chore(SwingTestCase): reformat code

--- src/test/java/de/danielluedecke/zettelkasten/SwingTestCase.java
+++ src/test/java/de/danielluedecke/zettelkasten/SwingTestCase.java
@@ -8,17 +8,18 @@ public class SwingTestCase extends TestCase {
     private JFrame testFrame;
 
     @Override
-    protected void tearDown(  ) {
+    protected void tearDown() {
         if (this.testFrame != null) {
-            this.testFrame.dispose( );
+            this.testFrame.dispose();
             this.testFrame = null;
         }
     }
 
-    public JFrame getTestFrame(  ) {
+    public JFrame getTestFrame() {
         if (this.testFrame == null) {
             this.testFrame = new JFrame("Test");
         }
         return this.testFrame;
     }
+
 }
