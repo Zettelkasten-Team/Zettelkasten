706e3c8cad52fd4923e68e95dd4dc4b288edf108
Ralf Barkow <ralf.barkow@me.com>
2020-11-19 07:15:30 +0100

    refactor(PersonEditorPanel): see https://resources.oreilly.com/examples/9780596003876/blob/master/junit/src/com/oreilly/javaxp/junit/PersonEditorPanel.java

--- src/main/java/de/danielluedecke/zettelkasten/PersonEditorPanel.java
+++ src/main/java/de/danielluedecke/zettelkasten/PersonEditorPanel.java
@@ -1,57 +1,70 @@
 package de.danielluedecke.zettelkasten;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class PersonEditorPanel extends JPanel {
     private JPanel PersonEditorPanel;
-    private JTextField firstNameField = new JTextField(20);
-    private JTextField lastNameField = new JTextField(20);
-    // @todo - add more fields later
-
+    private JTextField firstNameField;
+    private JTextField lastNameField;
     private Person person;
 
-    public PersonEditorPanel(JPanel personEditorPanel) {
-        layoutGui(  );
-        updateDataDisplay(  );
+    public PersonEditorPanel() {
+        layoutGui();
+        updateDataDisplay();
     }
 
-    public void setPerson(Person p) {
-        this.person = person;
-        updateDataDisplay(  );
+    JTextField getFirstNameField() {
+        return this.firstNameField;
     }
 
-    public Person getPerson(  ) {
-        // @todo - update the person with new information from the fields
-        return this.person;
+    JTextField getLastNameField() {
+        return this.lastNameField;
     }
 
-    private void layoutGui(  ) {
-        // @todo - define the layout
+    private void layoutGui() {
+        setLayout(new GridBagLayout());
+        GridBagConstraints gbc = new GridBagConstraints();
+        gbc.gridx = 0;
+        add(new JLabel("First Name:"), gbc);
+        add(new JLabel("Last Name:"), gbc);
+
+        gbc.gridx = 1;
+        gbc.weightx = 1.0;
+        gbc.fill = GridBagConstraints.HORIZONTAL;
+        add(this.firstNameField, gbc);
+        add(this.lastNameField, gbc);
     }
 
-    private void updateDataDisplay(  ) {
-        // @todo - ensure the fields are properly enabled, also set
-        //         data on the fields.
+    private void updateDataDisplay() {
         if (this.person == null) {
             this.firstNameField.setText("");
             this.lastNameField.setText("");
         } else {
-            this.firstNameField.setText(this.person.getFirstName(  ));
-            this.lastNameField.setText(this.person.getLastName(  ));
+            this.firstNameField.setText(this.person.getFirstName());
+            this.lastNameField.setText(this.person.getLastName());
         }
-        updateEnabledStates(  );
+        updateEnabledStates();
     }
 
-    JTextField getFirstNameField(  ) {
-        return this.firstNameField;
+    private void updateEnabledStates() {
+        if (this.person == null) {
+            this.firstNameField.setEnabled(false);
+            this.lastNameField.setEnabled(false);
+        } else {
+            this.firstNameField.setEnabled(true);
+            this.lastNameField.setEnabled(true);
+        }
     }
 
-    JTextField getLastNameField(  ) {
-        return this.lastNameField;
+    public void setPerson(Person person) {
+        this.person = person;
+        updateDataDisplay();
     }
 
-    private void updateEnabledStates(  ) {
-        this.firstNameField.setEnabled(person != null);
-        this.lastNameField.setEnabled(person != null);
+    public Person getPerson() {
+        // @todo - update the person with new information from the fields
+        return this.person;
     }
+
 }
