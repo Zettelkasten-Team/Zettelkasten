1fe6bee0327279fca3897f3257c13bc048c4f1d0
Ralf Barkow <>
2020-11-24 22:34:46 +0100

    chore(CImportBibTexTest): //TODO filter for the non-standard BibTeX field "abstract"

--- src/test/java/de/danielluedecke/zettelkasten/CImportBibTexTest.java
+++ src/test/java/de/danielluedecke/zettelkasten/CImportBibTexTest.java
@@ -23,6 +23,15 @@ class CImportBibTexTest {
 
     @Test
     void shouldCreateZettelIfBibTeXEntryHasAbstract() {
+        //TODO filter for the non-standard BibTeX field "abstract"
+        // - Stream: CreateZettelIfBibTeXEntryHasAbstract sClicked
+        // (an event is generated and fed into a stream called sClicked)
+        // - This event propagates to a map operation that transforms the Unit value into …
+        // (this map operation produces a new stream that we have called sClearIt.
+        // - The event in sClearIt propagates to the text field and changes its text contents to the contained value
+        // See https://livebook.manning.com/book/functional-reactive-programming/chapter-2/23
+        // 2.1. The Stream type: a stream of events
+
     }
 
     @Test
