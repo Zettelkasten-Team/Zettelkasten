4782f11422a8f1be8f202681a1ed6a0040a2ae9c
Ralf Barkow <>
2021-02-12 12:11:06 +0100

    test(HtmlUbbUtilTest): initial commit

---
+++ src/test/java/de/danielluedecke/zettelkasten/util/HtmlUbbUtilTest.java
@@ -0,0 +1,13 @@
+package de.danielluedecke.zettelkasten.util;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HtmlUbbUtilTest {
+
+    @Test
+    void getEntryAsHTML() {
+        StringBuilder retval = new StringBuilder();
+    }
+}
