7e2ba8d8b33349a7b1b52a445dcc24b13d2a843a
Ralf Barkow <>
2021-02-19 12:12:43 +0100

    feat(ObjectFactory):  can be used to create new instances of schema derived classes for package: ch.dreyeck.zettelkasten.entities This file was generated using the JavaTM Architecture for XML Binding(JAXB) Reference Implementation, v2.2.8-b130911.1802. Copied from https://github.com/RalfBarkow/ZettelkastenFX
    ( #330 )
    
    feat: represents a new feature, and correlates to a SemVer minor.

---
+++ src/main/java/ch/dreyeck/zettelkasten/xml/Links.java
@@ -0,0 +1,73 @@
+//
+// Diese Datei wurde mit der JavaTM Architecture for XML Binding(JAXB) Reference Implementation, v2.2.8-b130911.1802 generiert 
+// Siehe <a href="http://java.sun.com/xml/jaxb">http://java.sun.com/xml/jaxb</a> 
+// Änderungen an dieser Datei gehen bei einer Neukompilierung des Quellschemas verloren. 
+// Generiert: 2019.10.06 um 10:17:37 AM CEST 
+//
+
+
+package ch.dreyeck.zettelkasten.xml;
+
+import jakarta.xml.bind.annotation.XmlAccessType;
+import jakarta.xml.bind.annotation.XmlAccessorType;
+import jakarta.xml.bind.annotation.XmlRootElement;
+import jakarta.xml.bind.annotation.XmlType;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+/**
+ * <p>Java-Klasse für anonymous complex type.
+ *
+ * <p>Das folgende Schemafragment gibt den erwarteten Content an, der in dieser Klasse enthalten ist.
+ *
+ * <pre>
+ * &lt;complexType>
+ *   &lt;complexContent>
+ *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
+ *       &lt;sequence>
+ *         &lt;element ref="{}link" maxOccurs="unbounded" minOccurs="0"/>
+ *       &lt;/sequence>
+ *     &lt;/restriction>
+ *   &lt;/complexContent>
+ * &lt;/complexType>
+ * </pre>
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "", propOrder = {
+        "link"
+})
+@XmlRootElement(name = "links")
+public class Links {
+
+    protected List<String> link;
+
+    /**
+     * Gets the value of the link property.
+     *
+     * <p>
+     * This accessor method returns a reference to the live list,
+     * not a snapshot. Therefore any modification you make to the
+     * returned list will be present inside the JAXB object.
+     * This is why there is not a <CODE>set</CODE> method for the link property.
+     *
+     * <p>
+     * For example, to add a new item, do as follows:
+     * <pre>
+     *    getLink().add(newItem);
+     * </pre>
+     *
+     *
+     * <p>
+     * Objects of the following type(s) are allowed in the list
+     * {@link String }
+     */
+    public List<String> getLink() {
+        if (link == null) {
+            link = new ArrayList<String>();
+        }
+        return this.link;
+    }
+
+}
