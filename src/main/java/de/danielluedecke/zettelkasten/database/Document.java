c7b3039b7d1ec42b19085f6162b50c449dd56703
Ralf Barkow <>
2021-08-05 10:35:52 +0200

    test[Red]: create the class Document with stubbed-out methods
    Former-commit-id: b19b462f68a386fca9c010c21d57e78a8394d4ec

---
+++ src/main/java/de/danielluedecke/zettelkasten/database/Document.java
@@ -0,0 +1,17 @@
+package de.danielluedecke.zettelkasten.database;
+
+public class Document {
+
+    void getAuthor() {
+        // Create the class with stubbed-out methods.
+    }
+
+    void getTitle() {
+        // Create the class with stubbed-out methods.
+    }
+
+    void getYear() {
+        // Create the class with stubbed-out methods.
+    }
+
+}
