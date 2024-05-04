2b9d19edc295adc403f7f62c4569f391643c7508
RalfBarkow <>
2024-05-04 07:59:23 +0200

    test: Create playground for experimenting with Java code examples

---
+++ src/main/java/playground/Launcher.java
@@ -0,0 +1,13 @@
+package playground;
+
+import playground.JavaFXMenuExample;
+
+
+import javafx.application.Application;
+
+public class Launcher {
+    public static void main(String[] args) {
+        // Launch the JavaFX application
+        Application.launch(JavaFXMenuExample.class, args);
+    }
+}
