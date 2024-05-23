820f1bf2afe1aaf1e979ea802723afd190e59d0b
RalfBarkow <ralf.barkow@me.com>
2024-05-23 18:00:37 +0200

    test(ClickListener): journal or log view. See: #514
    In this example:
    
    1. UI Setup:
    
    * We create a JFrame to serve as the main window.
    * A JPanel holds two buttons (button1 and button2).
    * A JTextArea (logArea) is used to display the log of click events. This text area is placed inside a JScrollPane to make it scrollable.
    
    2. Event Listener:
    
    * The ClickListener class implements ActionListener and has a reference to the JTextArea used for logging.
    * In the actionPerformed method, we determine the source of the event, construct a log message, and append this message to the JTextArea.
    
    3. Event Logging:
    
    * When a button is clicked, the ClickListener logs the event by adding a new line to the JTextArea.
    
    This setup allows you to dynamically log all click events along with the related UI component, similar to a journal or log view. You can extend this by adding more components and listeners as needed.

--- src/main/java/playground/ClickLogger.java
+++ src/main/java/playground/ClickLogger.java
@@ -1,40 +1,55 @@
 package playground;
 
+import java.awt.BorderLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.*;
 
 public class ClickLogger {
-	public static void main(String[] args) {
-		JFrame frame = new JFrame("Click Logger");
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-		JPanel panel = new JPanel();
-
-		JButton button1 = new JButton("Button 1");
-		JButton button2 = new JButton("Button 2");
-
-		button1.addActionListener(new ClickListener());
-		button2.addActionListener(new ClickListener());
-
-		panel.add(button1);
-		panel.add(button2);
-
-		frame.getContentPane().add(panel);
-		frame.pack();
-		frame.setVisible(true);
-	}
-
-	static class ClickListener implements ActionListener {
-		@Override
-		public void actionPerformed(ActionEvent e) {
-			Object source = e.getSource();
-			if (source instanceof JButton) {
-				JButton button = (JButton) source;
-				System.out.println("Click event on " + button.getText());
-			} else {
-				System.out.println("Click event on unknown component");
-			}
-		}
-	}
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Click Logger");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(400, 300);
+        
+        JPanel panel = new JPanel();
+        
+        JButton button1 = new JButton("Button 1");
+        JButton button2 = new JButton("Button 2");
+        
+        JTextArea logArea = new JTextArea(10, 30);
+        logArea.setEditable(false);
+        
+        button1.addActionListener(new ClickListener(logArea));
+        button2.addActionListener(new ClickListener(logArea));
+        
+        panel.add(button1);
+        panel.add(button2);
+        
+        frame.getContentPane().add(panel, BorderLayout.NORTH);
+        frame.getContentPane().add(new JScrollPane(logArea), BorderLayout.CENTER);
+        
+        frame.setVisible(true);
+    }
+    
+    static class ClickListener implements ActionListener {
+        private JTextArea logArea;
+        
+        public ClickListener(JTextArea logArea) {
+            this.logArea = logArea;
+        }
+        
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            Object source = e.getSource();
+            String logMessage;
+            if (source instanceof JButton) {
+                JButton button = (JButton) source;
+                logMessage = "Click event on " + button.getText();
+            } else {
+                logMessage = "Click event on unknown component";
+            }
+            logArea.append(logMessage + "\n");
+        }
+    }
 }
+
