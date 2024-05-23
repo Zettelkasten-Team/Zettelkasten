711c7a59e7fd28c3c45913538eedf4aa1fa0d698
RalfBarkow <ralf.barkow@me.com>
2024-05-23 14:19:39 +0200

    test(ClickListener): log the source of the event. See: #514
    To log the source of the event (i.e., the component that triggered the event) in Java, you can use the getSource() method from the ActionEvent class. This method returns the object that fired the event, which you can then log.
    
    In this updated example:
    
    * The ClickListener no longer takes the component name in the constructor. Instead, it determines the source of the event dynamically using e.getSource().
    
    * e.getSource() returns the object that triggered the event, which we check to see if it's an instance of JButton.
    
    * If the source is a JButton, we cast it to a JButton and log its text. This way, the code can handle any number of buttons without hardcoding their names.
    
    Now, whenever a button is clicked, the text of the button that triggered the event is logged. This approach is more flexible and can easily be extended to other types of components.

--- src/main/java/playground/ClickLogger.java
+++ src/main/java/playground/ClickLogger.java
@@ -5,38 +5,36 @@ import java.awt.event.ActionListener;
 import javax.swing.*;
 
 public class ClickLogger {
-    public static void main(String[] args) {
-        JFrame frame = new JFrame("Click Logger");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	public static void main(String[] args) {
+		JFrame frame = new JFrame("Click Logger");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        JPanel panel = new JPanel();
+		JPanel panel = new JPanel();
 
-        JButton button1 = new JButton("Button 1");
-        JButton button2 = new JButton("Button 2");
+		JButton button1 = new JButton("Button 1");
+		JButton button2 = new JButton("Button 2");
 
-        button1.addActionListener(new ClickListener("Button 1"));
-        button2.addActionListener(new ClickListener("Button 2"));
+		button1.addActionListener(new ClickListener());
+		button2.addActionListener(new ClickListener());
 
-        panel.add(button1);
-        panel.add(button2);
+		panel.add(button1);
+		panel.add(button2);
 
-        frame.getContentPane().add(panel);
-        frame.pack();
-        frame.setVisible(true);
-    }
+		frame.getContentPane().add(panel);
+		frame.pack();
+		frame.setVisible(true);
+	}
 
-    static class ClickListener implements ActionListener {
-        private String componentName;
-
-        public ClickListener(String componentName) {
-            this.componentName = componentName;
-        }
-
-        @Override
-        public void actionPerformed(ActionEvent e) {
-            System.out.println("Click event on " + componentName);
-            // You can log more information here if needed
-        }
-    }
+	static class ClickListener implements ActionListener {
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			Object source = e.getSource();
+			if (source instanceof JButton) {
+				JButton button = (JButton) source;
+				System.out.println("Click event on " + button.getText());
+			} else {
+				System.out.println("Click event on unknown component");
+			}
+		}
+	}
 }
-
