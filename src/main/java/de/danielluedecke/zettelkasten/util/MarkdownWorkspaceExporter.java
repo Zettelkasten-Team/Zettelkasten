b8c74ee6904a9a878ba2d6ace35708ffaa397134
rgb <ralf.barkow@me.com>
2026-01-25 11:57:46 +0100

    fix(export): log markdown export success at INFO

--- src/main/java/de/danielluedecke/zettelkasten/util/MarkdownWorkspaceExporter.java
+++ src/main/java/de/danielluedecke/zettelkasten/util/MarkdownWorkspaceExporter.java
@@ -109,7 +109,7 @@ public final class MarkdownWorkspaceExporter {
 				Constants.zknlogger.log(Level.WARNING, "Pandoc export failed for entry {0} (exit code {1}).",
 						new Object[] { entryNumber, exitCode });
 			} else {
-				Constants.zknlogger.log(Level.WARNING, "Markdown export created: {0}",
+				Constants.zknlogger.log(Level.INFO, "Markdown export created: {0}",
 						outFile.toAbsolutePath().toString());
 			}
 		} catch (InterruptedException ex) {
